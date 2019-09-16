bfdc03851dec40782afd60b26c00a801b434d8d6
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-17 00:36:07 +1200

    Deployment Testing

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use('/sendEmail', sendEmailRouter);
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-if (process.env.NOD_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '../client/build/index.html'));
+        res.sendFile(path.join(__dirname + 'client', 'build', 'index.html'));
     });
 }
 
