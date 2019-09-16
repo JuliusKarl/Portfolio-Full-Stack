4ebd25e120a7655a72883c082a797dc0f1b188fb
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-17 00:26:08 +1200

    Deployment Testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json())//
 
 const sendEmailRouter = require('./routes/sendEmail');
 app.use('/sendEmail', sendEmailRouter);
