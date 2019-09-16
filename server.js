9ca9617976cd2cc86c269f13bb9fa1b58483bcd9
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-16 23:59:35 +1200

    Deployment Testing

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
+if (process.env.NOD_ENV === 'production') {
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + '../client/build/index.html'));
     });
 }
