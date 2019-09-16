eeebdb9816e1ee42a56a70542001ca1e04d6baba
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-16 15:31:12 +1200

    Trying to deploy

--- server.js
+++ server.js
@@ -16,15 +16,11 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-});
-
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
