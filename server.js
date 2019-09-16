9d2a302645867b55958c2cd496cb4d83c91c58a0
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-16 22:45:51 +1200

    Deployment Testing

--- server.js
+++ server.js
@@ -12,15 +12,19 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-/*
+
 app.get('/', (req, res) => {
     res.send('Hello');
 });
-*/
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const sendEmailRouter = require('./routes/sendEmail');
 app.use('/sendEmail', sendEmailRouter);
 
