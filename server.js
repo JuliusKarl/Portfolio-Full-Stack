87f67ad73e17eff8e5fee27febcf83e2b009f3bc
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-09-16 22:28:18 +1200

    Deployment Testing

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json())
-
-const path = require("path");
-app.use(express.static(path.join(__dirname, "client/build")))
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+/*
 app.get('/', (req, res) => {
     res.send('Hello');
 });
-
-
-if (process.env.NOD_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));
-    });
-}
+*/
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 const sendEmailRouter = require('./routes/sendEmail');
 app.use('/sendEmail', sendEmailRouter);
 
